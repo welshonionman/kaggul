dbfc457c3184c59bcb78d53192f69f6317681450
welshonionman <welshonionman@gmail.com>
2024-10-05 20:39:57 +0900

    split kaggle_utils.py

--- myka/kaggle_util.py
+++ myka/kaggle_util.py
@@ -4,11 +4,8 @@ import os
 import random
 import shutil
 import subprocess
-import time
-from datetime import datetime, timezone
-from pathlib import Path
 
-from kaggle.api.kaggle_api_extended import KaggleApi
+from pathlib import Path
 
 
 def init(kaggle_json_path: str) -> None:
@@ -205,38 +202,3 @@ def submission(comp_name: str, file_path: str, description: str = "submission") 
         f"kaggle competitions submit {comp_name} -f {file_path} -m {description}"
     )
 
-
-def measure_submission_elapsed_time(competition: str, idx: str = 0) -> None:
-    """
-    サブミットを行った時からの経過時間を計測します。
-    idxには最新のサブミットから数えて何番目のサブミットかを指定します。
-    idx = 0(default)の場合は最新のサブミットを指定します。
-
-    Args:
-        competition (str): コンペティション名
-        idx (str): サブミット番号 (default: 0)
-    """
-    api = KaggleApi()
-    api.authenticate()
-
-    result_ = api.competition_submissions(competition)[idx]
-    latest_ref = str(result_)  # 最新のサブミット番号
-    submit_time = result_.date
-    description = result_.description
-    print(f"submission description: {description}")
-
-    status = ""
-    while status != "complete":
-        list_of_submission = api.competition_submissions(competition)
-        for result in list_of_submission:
-            if str(result.ref) == latest_ref:
-                break
-        status = result.status
-
-        now = datetime.datetime.now(timezone.utc).replace(tzinfo=None)
-        elapsed_time = int((now - submit_time).seconds / 60) + 1
-        if status == "complete":
-            print("\r" f"run-time: {elapsed_time} min, LB: {result.publicScore}")
-        else:
-            print("\r", f"elapsed time: {elapsed_time} min", end="")
-            time.sleep(60)
---
+++ myka/measure.py
@@ -0,0 +1,41 @@
+import time
+from datetime import datetime, timezone
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+def submission_elapsed(competition: str, idx: str = 0) -> None:
+    """
+    サブミットを行った時からの経過時間を計測します。
+    idxには最新のサブミットから数えて何番目のサブミットかを指定します。
+    idx = 0(default)の場合は最新のサブミットを指定します。
+
+    Args:
+        competition (str): コンペティション名
+        idx (str): サブミット番号 (default: 0)
+    """
+
+    api = KaggleApi()
+    api.authenticate()
+
+    result_ = api.competition_submissions(competition)[idx]
+    latest_ref = str(result_)  # 最新のサブミット番号
+    submit_time = result_.date
+    description = result_.description
+    print(f"submission description: {description}")
+
+    status = ""
+    while status != "complete":
+        list_of_submission = api.competition_submissions(competition)
+        for result in list_of_submission:
+            if str(result.ref) == latest_ref:
+                break
+        status = result.status
+
+        now = datetime.datetime.now(timezone.utc).replace(tzinfo=None)
+        elapsed_time = int((now - submit_time).seconds / 60) + 1
+        if status == "complete":
+            print("\r" f"run-time: {elapsed_time} min, LB: {result.publicScore}")
+        else:
+            print("\r", f"elapsed time: {elapsed_time} min", end="")
+            time.sleep(60)
